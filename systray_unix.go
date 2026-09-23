3f2ed26e78d8f87c2ddee1224286a3239e1a2e2e
shansing <i@shansing.com>
2026-09-23 14:33:36 +0800

    fix(unix): preserve translucent tray icon colors

--- systray_unix.go
+++ systray_unix.go
@@ -11,6 +11,7 @@ import (
 	"bytes"
 	"fmt"
 	"image"
+	"image/color"
 	_ "image/png" // used only here
 	"log"
 	"os"
@@ -445,11 +446,11 @@ func argbForImage(img image.Image) []byte {
 	i := 0
 	for y := 0; y < h; y++ {
 		for x := 0; x < w; x++ {
-			r, g, b, a := img.At(x, y).RGBA()
-			data[i] = byte(a)
-			data[i+1] = byte(r)
-			data[i+2] = byte(g)
-			data[i+3] = byte(b)
+			c := color.NRGBAModel.Convert(img.At(x, y)).(color.NRGBA)
+			data[i] = c.A
+			data[i+1] = c.R
+			data[i+2] = c.G
+			data[i+3] = c.B
 			i += 4
 		}
 	}
